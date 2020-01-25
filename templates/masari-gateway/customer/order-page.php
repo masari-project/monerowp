9e7668b22553ff3c6bb1262347d0205b5870bed9
camthegeek <cam@camthegeek.net>
2020-01-25 17:22:55 -0500

    use inputs instead of list items (left out from init commit); fix styling

--- templates/masari-gateway/customer/order-page.php
+++ templates/masari-gateway/customer/order-page.php
@@ -17,57 +17,50 @@
         <span class="masari_payment_expired_partial">Your order has expired. Please contact the store owner to receive refund on your partial payment.</span>
 
     </strong>
+    <!-- this section strongly inspired by gnock of Masari -->
+    <div class="masari_payment_send_info">
+        <div class="data-box">
+            <label>Amount</label>
+            <input id="masari_total_due" type="text" readonly class="value" value="">
+            <button href="#" class="button copy clipboard" title="Copy Amount" data-clipboard-target="#masari_total_due">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512" version="1"><path d="M504 118c-6-6-12-8-20-8H365c-11 0-23 3-36 11V27c0-7-3-14-8-19s-12-8-20-8H183c-8 0-16 2-25 6-10 4-17 8-22 13L19 136c-5 5-9 12-13 22-4 9-6 17-6 25v192c0 7 3 14 8 19s12 8 19 8h156v82c0 8 2 14 8 20 5 5 12 8 19 8h274c8 0 14-3 20-8 5-6 8-12 8-20V137c0-8-3-14-8-19zm-175 52v86h-85l85-86zM146 61v85H61l85-85zm56 185c-5 5-10 12-14 21-3 9-5 18-5 25v73H37V183h118c8 0 14-3 20-8 5-6 8-12 8-20V37h109v118l-90 91zm273 229H219V292h119c8 0 14-2 19-8 6-5 8-11 8-19V146h110v329z"/></svg>
+            </button>
+        </div>
+        <div class="data-box">
+            <label>Address</label>
+            <input id="masari_integrated_address" type="text" readonly class="value" value="">
+            <button href="#" class="button copy clipboard" title="Copy Address" data-clipboard-target="#masari_integrated_address">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512" version="1"><path d="M504 118c-6-6-12-8-20-8H365c-11 0-23 3-36 11V27c0-7-3-14-8-19s-12-8-20-8H183c-8 0-16 2-25 6-10 4-17 8-22 13L19 136c-5 5-9 12-13 22-4 9-6 17-6 25v192c0 7 3 14 8 19s12 8 19 8h156v82c0 8 2 14 8 20 5 5 12 8 19 8h274c8 0 14-3 20-8 5-6 8-12 8-20V137c0-8-3-14-8-19zm-175 52v86h-85l85-86zM146 61v85H61l85-85zm56 185c-5 5-10 12-14 21-3 9-5 18-5 25v73H37V183h118c8 0 14-3 20-8 5-6 8-12 8-20V37h109v118l-90 91zm273 229H219V292h119c8 0 14-2 19-8 6-5 8-11 8-19V146h110v329z"/></svg>
+            </button>           
+        </div>
+        
+        <?php if($show_qr): ?>
+        <div class="qr-code">
+            <div id="masari_qr_code"></div>
+        </div>
+         <?php endif; ?>
 
-    <ul class="order_details" style="margin-top:30px">
-        <li>
-            Pay to:
-            <strong class="masari_details_row">
-                <span class="masari_details_main" id="masari_integrated_address"></span>
-                <span class="masari_details_right button-row">
-                    <?php if($show_qr): ?>
-                    <button href="#" class="button" title="Show QR Code" onclick="masari_showQR()">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512" version="1"><path d="M0 512h233V279H0zm47-186h139v139H47z"/><path d="M93 372h47v47H93zm279 93h47v47h-47zm93 0h47v47h-47z"/><path d="M465 326h-46v-47H279v233h47V372h46v47h140V279h-47zM0 233h233V0H0zM47 47h139v139H47z"/><path d="M93 93h47v47H93zM279 0v233h233V0zm186 186H326V47h139z"/><path d="M372 93h47v47h-47z"/></svg>
-                    </button>
-                    <?php endif; ?>
-                    <button href="#" class="button clipboard" title="Copy Address"
-                            data-clipboard-target="#masari_integrated_address">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512" version="1"><path d="M504 118c-6-6-12-8-20-8H365c-11 0-23 3-36 11V27c0-7-3-14-8-19s-12-8-20-8H183c-8 0-16 2-25 6-10 4-17 8-22 13L19 136c-5 5-9 12-13 22-4 9-6 17-6 25v192c0 7 3 14 8 19s12 8 19 8h156v82c0 8 2 14 8 20 5 5 12 8 19 8h274c8 0 14-3 20-8 5-6 8-12 8-20V137c0-8-3-14-8-19zm-175 52v86h-85l85-86zM146 61v85H61l85-85zm56 185c-5 5-10 12-14 21-3 9-5 18-5 25v73H37V183h118c8 0 14-3 20-8 5-6 8-12 8-20V37h109v118l-90 91zm273 229H219V292h119c8 0 14-2 19-8 6-5 8-11 8-19V146h110v329z"/></svg>
-                    </button>
-                </span>
-            </strong>
-        </li>
-        <li>
-            Total due:
-            <strong class="masari_details_row">
-                <span class="masari_details_main">
-                    <span id="masari_total_due"></span> MSR
-                </span>
-                <span class="masari_details_right button-row">
-                    <button href="#" class="button clipboard" title="Copy Amount"
-                            data-clipboard-target="#masari_total_due">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512" version="1"><path d="M504 118c-6-6-12-8-20-8H365c-11 0-23 3-36 11V27c0-7-3-14-8-19s-12-8-20-8H183c-8 0-16 2-25 6-10 4-17 8-22 13L19 136c-5 5-9 12-13 22-4 9-6 17-6 25v192c0 7 3 14 8 19s12 8 19 8h156v82c0 8 2 14 8 20 5 5 12 8 19 8h274c8 0 14-3 20-8 5-6 8-12 8-20V137c0-8-3-14-8-19zm-175 52v86h-85l85-86zM146 61v85H61l85-85zm56 185c-5 5-10 12-14 21-3 9-5 18-5 25v73H37V183h118c8 0 14-3 20-8 5-6 8-12 8-20V37h109v118l-90 91zm273 229H219V292h119c8 0 14-2 19-8 6-5 8-11 8-19V146h110v329z"/></svg>
-                    </button>
-                </span>
-            </strong>
-        </li>
-        <li style="display:none">
-            Total order amount:
-            <strong>
-                <span id="masari_total_amount"></span> MSR
-            </strong>
-        </li>
-        <li>
-            Total paid:
-            <strong>
-                <span id="masari_total_paid"></span> MSR
-            </strong>
-        </li>
-        <li>
-            Exchange rate:<strong id="masari_exchange_rate"></strong>
-        </li>
-    </ul>
-
-
+    </div>          
+    
+    <div class="data-box">  
+        <ul>
+            <li style="display:none">
+                Total order amount:
+                <strong>
+                    <span id="masari_total_amount"></span> MSR
+                </strong>
+            </li>
+            <li>
+                Total paid:
+                <strong>
+                    <span id="masari_total_paid"></span> MSR
+                </strong>
+            </li>
+            <li>
+                Exchange rate:<strong id="masari_exchange_rate"></strong>
+            </li>
+        </ul>
+    </div>
 
     <table id="masari_tx_table" style="display:none;">
         <thead>
@@ -81,11 +74,6 @@
         </tbody>
     </table>
     <div id="masari_tx_none" style="display:none;">
-    </div>
-
-    <div id="masari_qr_code_container" style="display:none;" onclick="masari_showQR(false)">
-        <div id="masari_qr_code">
-        </div>
     </div>
 
 </section>
